1f4a0db30ec07ccfc7cc668d2e368c4d8039023b
jrodrigomg <jrodrigomg@hotmail.com>
2018-04-26 20:13:43 -0600

    Blotter config

--- js/app.js
+++ js/app.js
@@ -17,19 +17,53 @@ $(document).ready(function() {
 
 
 function inicializarBlotter(){
-	var text = new Blotter.Text("Acerca", {
-		family : "serif",
-		size : 90,
+	var text = new Blotter.Text("Acerca de mi:", {
+		family : "MyWebFont",
+		size : 100,
 		fill : "#171717"
 	});
 
 	var material = new Blotter.ChannelSplitMaterial();
 
+	material.uniforms.uOffset.value= 0.070;
+	material.uniforms.uRotation.value = 138;
+	material.uniforms.uApplyBlur.value = 1;
+	material.uniforms.uAnimateNoise.value = 1;
 	var blotter = new Blotter(material, { texts : text });
+
 
 	var scope = blotter.forText(text);
 
-		scope.appendTo(document.getElementById("blotterTitleSectionTwo"));
+	scope.appendTo(document.getElementById("blotterTitleSectionTwo"));
+
+	var elem = $( ".title-section-2 > .b-canvas");
+	var element_x = elem.position().left;
+	var element_y = elem.position().top + 1;
+	var body_x = $(document).width();
+	var body_y = $(document).height();
+	// blotter.needsUpdate = true;
+	$( document ).on( "mousemove", function( event ) {
+			var cursor_x = event.pageX;
+			var cursor_y = event.pageY;
+			var diff_x = Math.abs(cursor_x - element_x);
+			var diff_y = Math.abs(cursor_y - element_y);
+			var porcent_x = diff_x / body_x;
+			var porcent_y = diff_y / body_y;
+			var rotation = 90 * porcent_x;
+			if(cursor_x > element_x)
+				rotation = 180*porcent_x;
+
+			var offset = 0.800*porcent_y;
+
+
+			// var diff = Math.floor(Math.sqrt(Math.pow(cursor_x - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(cursor_y - (elem.offset().top+(elem.height()/2)), 2)));
+
+			material.uniforms.uOffset.value= offset;
+			material.uniforms.uRotation.value = rotation;
+			scope.render();
+
+	});
+
 }
 
 
